77a83d7f3932731ae93d0d575b55595a40c0e9b0
Sneha-14-hub <yourselflove9760@hmail.com>
2025-12-20 23:03:00 +0530

    Initial local commit

---
+++ app/LandingPage/page.jsx
@@ -0,0 +1,13 @@
+import {Navbar1} from "@/components/ui/navbar1"
+import Collaborators from "./Collaborators"
+import Home from "./Home"
+export default  function LandingPage(){
+    
+    return(
+        <>
+        <Navbar1/>
+        < Home />
+      <Collaborators />
+        </>
+    )
+}
