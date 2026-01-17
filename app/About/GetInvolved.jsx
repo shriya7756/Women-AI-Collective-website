10b1b0c593ff3bad6738089acd12d491fe966547
sneha_dubey <yourselflove9760@gmail.com>
2026-01-17 16:10:04 +0530

    final project code

---
+++ app/About/GetInvolved.jsx
@@ -0,0 +1,83 @@
+export default function GetInvolved() {
+  return (
+ <section className="bg-[#0B0B1A] py-24 px-6">
+<h1 className="font-['Work_Sans'] text-[50px] font-bold leading-[59px] text-center text-[#FAFCFE] max-w-[356px] mx-auto whitespace-nowrap mb-[30px]">
+  GET INVOLVED </h1>
+
+<div className="max-w-[1400px] mx-auto grid grid-cols-1 md:grid-cols-3 gap-10">
+<div className="top-[50px] left-[40px] w-[420px] h-[410px] bg-[#F1247B] rounded-[40px] p-10 text-white flex flex-col justify-between gap-[10px]"> 
+     <div>
+    <h2 className="font-['Work_Sans'] text-[40px] font-semibold leading-[45px] flex items-center justify-center gap-[10px] text-center">
+    Join the <br/>WAIC team </h2>
+
+    <div className="flex items-center justify-center">
+    <p className="mt-[14px] font-['Work_Sans'] text-[18px] font-normal leading-[28px] text-center text-[#ECF1F4] max-w-[320px] whitespace-pre-line">
+    Be part of the team building
+    Women AI Collective from the
+    ground up. Join us accross
+    community, content, research, tech
+    and operations – and grow while
+    creating access to more women.</p> </div>
+    </div>
+
+ <div className="flex justify-center">
+  <button className="flex items-center gap-[8px] bg-white hover:bg-[#FAFCFE] rounded-[8px] border border-[#0F0F0F] px-[16px] py-[6px] w-fit h-fit" >
+    <span className="font-['Work_Sans'] text-[16px] font-medium text-[#06061A]">
+      Apply Now
+    </span>
+    <img src="/Pixel Arrow.png" alt="Arrow" className="w-[20px] h-[20px]" />
+  </button>
+</div>
+        </div>
+       
+ <div className=" top-[50px] left-[40px] right-[40px] bottom-[50px] w-[420px] h-[440px] bg-[#F5C939] rounded-[40px] p-10 text-black flex flex-col justify-between gap-[10px]">
+  <div>
+   <h2 className=" font-['Work_Sans'] text-[40px] font-semibold leading-[45px] text-center text-[#2E2E2E] max-w-[340px] ">
+    Conduct a Guest <br />Lecture Session</h2>
+
+<p className="mt-[14px] font-['Work_Sans'] text-[18px] font-normal leading-[28px] text-center text-[#333333] max-w-[350px] whitespace-pre-line">
+  If you are a professional working in
+  emerging tech and AI,we welcome you
+  to mentor young women virtually. Lead
+  a focused 60–90 minute session as
+  part of our program and inspire
+  learners through real-world insights
+  and practical guidance.</p>
+</div>
+ <div className="flex justify-center mt-4">
+  <button className="flex items-center bg-white hover:bg-[#FAFCFE] rounded-[8px] border border-[#0F0F0F] px-[16px] py-[6px] w-fit h-fit" >
+    <span className="font-['Work_Sans'] text-[16px] font-medium text-[#06061A]">
+     Get in Touch
+    </span>
+    <img src="/Pixel Arrow.png" alt="Arrow" className="w-[20px] h-[20px]" />
+  </button>
+</div>
+        </div>
+      
+<div className="top-[50px] left-[40px] right-[40px] bottom-[50px] w-[420px] h-[410px] bg-[#8A38F5] rounded-[40px] p-10 text-white flex flex-col justify-between gap-[10px]">
+<div>
+<h2 className="font-['Work_Sans'] text-[40px] font-semibold leading-[45px] flex items-center justify-center gap-[10px] text-center">
+ Be a WAIC Ambassador</h2>
+
+<p className="mt-[14px] font-['Work_Sans'] text-[18px] font-normal leading-[28px] text-center text-[#ECF1F4] max-w-[320px] whitespace-pre-line">
+Shape an inclusive digital future by 
+bringing Women AI Collective to your 
+college, school, or community. As an 
+ambassador, you’ll help more girls 
+discover AI, start conversations, and 
+open doors to learning opportunities.</p>
+          </div>
+          <div className="flex justify-center">
+  <button className="flex items-center gap-[8px] bg-white hover:bg-[#FAFCFE] rounded-[8px] border border-[#0F0F0F] px-[16px] py-[6px] w-fit h-fit" >
+    <span className="font-['Work_Sans'] text-[16px] font-medium text-[#06061A]">
+     Apply Now
+    </span>
+    <img src="/Pixel Arrow.png" alt="Arrow" className="w-[20px] h-[20px]" />
+  </button>
+</div>
+        </div>
+
+      </div>
+    </section>
+  );
+}
